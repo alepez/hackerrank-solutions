3f81e55041776cb48f5b1705509dd4fc685873c5
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2019-05-30 08:19:52 +0200

    Add solution for sherlock and pairs

--- sherlock-and-pairs/sherlock-and-pairs_sort.hs
+++ sherlock-and-pairs/sherlock-and-pairs_sort.hs
@@ -1,11 +1,11 @@
 import Data.List (group, sort)
 
-doSomething :: [Int] -> Int
-doSomething =
+countPairs :: [Int] -> Int
+countPairs =
   sum . fmap (\x -> x * (x - 1)) . filter (> 1) . fmap length . group . sort
 
 main =
   interact $
   unlines .
-  fmap (show . doSomething . fmap read . words . snd) .
+  fmap (show . countPairs . fmap read . words . snd) .
   filter fst . zip (concat $ repeat [False, True]) . tail . lines
