2c77f9e3c3ea0bb73fd231b62ac9b701f0be8b42
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2019-09-06 00:50:41 +0200

    Fix solution

--- minimum-index-difference-1/minimum-index-difference-1.rs
+++ minimum-index-difference-1/minimum-index-difference-1.rs
@@ -1,11 +1,23 @@
 fn parse_line(line: &str) -> Vec<i32> {
-    line.split(' ').map(|x| x.parse().unwrap()).collect()
+    line.trim().split(' ').map(|x| x.parse().unwrap()).collect()
 }
 
 fn enumerate_and_sort(vec: Vec<i32>) -> Vec<(i32, i32)> {
     let mut vec: Vec<(usize, &i32)> = vec.iter().enumerate().collect();
-    vec.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    vec.sort_by(|a, b| a.1.cmp(&b.1));
     vec.iter().map(|x| (x.0 as i32, *x.1)).collect()
+}
+
+fn minimum_index_differnce(fst: Vec<i32>, snd: Vec<i32>) -> i32 {
+    let fst = enumerate_and_sort(fst);
+    let snd = enumerate_and_sort(snd);
+
+    *fst.iter()
+        .zip(snd.iter())
+        .map(|((i, x), (j, _))| (x, (i - j).abs()))
+        .min_by(|d1, d2| d1.1.cmp(&d2.1))
+        .unwrap()
+        .0
 }
 
 fn main() {
@@ -15,14 +27,8 @@ fn main() {
 
     let mut lines = stdin.lock().lines().map(Result::unwrap).skip(1);
 
-    let one = enumerate_and_sort(parse_line(lines.next().unwrap().as_str()));
-    let two = enumerate_and_sort(parse_line(lines.next().unwrap().as_str()));
+    let fst = parse_line(lines.next().unwrap().as_str());
+    let snd = parse_line(lines.next().unwrap().as_str());
 
-    let sum: i32 = one
-        .iter()
-        .zip(two.iter())
-        .map(|(x, y)| (x.0 - y.0).abs())
-        .sum();
-
-    println!("{:?}", sum / 2);
+    println!("{:?}", minimum_index_differnce(fst, snd));
 }
