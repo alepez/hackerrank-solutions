1c0b9f00851949f157a8bdd12c7ae651fbb8bb6e
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-03-14 23:38:05 +0100

    Add two-arrays_0.hs

---
+++ two-arrays/two-arrays_0.hs
@@ -0,0 +1,30 @@
+import Data.List
+
+main = interact $ ser . compute . de
+
+de :: String -> [(Int, [Int], [Int])]
+de s = fmap readGroup $ groupN 3 $ drop 1 $ lines s
+
+ser :: [Bool] -> String
+ser = unlines . fmap yesNo
+
+yesNo True = "YES"
+yesNo False = "NO"
+
+groupN :: Int -> [a] -> [[a]]
+groupN _ [] = []
+groupN n l = (take n l) : (groupN n (drop n l))
+
+readGroup :: [String] -> (Int, [Int], [Int])
+readGroup lines = (k, as, bs)
+  where
+    [_, k] = fmap read $ words $ head lines
+    as = fmap read $ words $ head $ drop 1 $ lines
+    bs = fmap read $ words $ head $ drop 2 $ lines
+
+compute :: [(Int, [Int], [Int])] -> [Bool]
+compute = fmap computeGroup
+
+computeGroup :: (Int, [Int], [Int]) -> Bool
+computeGroup (k, as, bs) =
+  all (>= k) $ zipWith (+) (sort as) (reverse $ sort bs)
