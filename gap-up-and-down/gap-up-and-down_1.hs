f490dc3edc4c52dabb73bca59fe8f170a6da26a5
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-03-17 14:17:23 +0100

    Update gap-up-and-down_1.hs

--- gap-up-and-down/gap-up-and-down_1.hs
+++ gap-up-and-down/gap-up-and-down_1.hs
@@ -1,19 +1,16 @@
 main = interact $ ser . compute . de
 
-de :: String -> [((Int, Int), Int)]
-de s = zip (zip lows highs) closes
+de s = zip3 lows highs closes
   where
     [lows, highs, closes] = (fmap read) <$> words <$> (drop 1 $ lines s)
 
-ser :: (Int, Int) -> String
 ser (gapUp, gapDown) = unwords . map show $ [gapUp, gapDown]
 
-compute :: [((Int, Int), Int)] -> (Int, Int)
-compute xs = (length gapUp, length gapDown)
+compute :: [(Int, Int, Int)] -> (Int, Int)
+compute xs = (length $ gap up, length $ gap down)
   where
-    gapDown = filter isGapLow z
-    gapUp = filter isGapUp z
-    z = zip (snd <$> xs) (fst <$> (tail xs))
-
-isGapLow (pc, (_, h)) = h < pc
-isGapUp (pc, (l, _)) = l > pc
+    lh = [(l, h) | (l, h, c) <- xs]
+    c = [c | (l, h, c) <- xs]
+    gap op = filter (uncurry op) $ zip (tail lh) c
+    up (l, h) c = l > c
+    down (l, h) c = h < c
